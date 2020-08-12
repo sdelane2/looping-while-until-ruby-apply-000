b00d9a29f1e4ea0f40da6561fac75eb5fbca1935
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-12 06:39:40 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until(phrase)
-counter = 6
-until counter == 10 
-  puts "Wingardium Leviosa"
-  counter += 1 
+  counter = 6
+  until counter == 10 
+    puts "Wingardium Leviosa"
+    counter += 1
+  end
 end
