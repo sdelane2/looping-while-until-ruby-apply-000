80e7bd47c8901e22112de2924d0f0b56a5a33e61
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-12 06:43:30 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,8 @@
-phrase = "Wingardium Leviosa"
 def using_while
   levitation_force = 6
   while levitation_force < 10
-    puts phrase
-    counter += 1
+    puts "Wingardium Leviosa"
+    levitation_force += 1
   end
 end
 
